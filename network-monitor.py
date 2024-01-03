d577687a424c6ac4acc2b01fc90f2d252efd2370
Ivan <IvanKatauzov@gmail.com>
2024-01-03 05:34:29 +0300

    adding saving packages

--- network-monitor.py
+++ network-monitor.py
@@ -1,14 +1,15 @@
 from scapy.all import sniff
 from scapy.layers.inet import TCP, IP
 from collections import defaultdict
-import time
+import threading
+import json
 
 
 class NetworkMonitor:
     def __init__(self):
         self.activity = defaultdict(lambda: defaultdict(int))
         self.suspicious_packets = []
-        self.start_time = time.time()
+        self.monitoring = True
 
     def process_packet(self, packet):
         if IP in packet and TCP in packet:
@@ -17,7 +18,7 @@ class NetworkMonitor:
             self.activity[src_ip][dst_port] += 1
 
             if self.is_suspicious_packet(packet):
-                print(f"[!] Потенциально подозрительный пакет от {src_ip} к порту {dst_port}")
+                print(f"Обнаружен подозрительный пакет: IP {src_ip}, Порт {dst_port}")
                 self.suspicious_packets.append((src_ip, dst_port))
 
     def is_suspicious_packet(self, packet):
@@ -31,22 +32,38 @@ class NetworkMonitor:
         return self.activity[src_ip][dst_port] > threshold
 
     def start(self):
-        print("Запуск мониторинга сетевого трафика...")
-        sniff(prn=self.process_packet, store=False)
+        print("Мониторинг запущен. Нажмите 'q', чтобы остановить.")
+        sniff_thread = threading.Thread(
+            target=lambda: sniff(prn=self.process_packet, store=False, stop_filter=lambda x: not self.monitoring))
+        sniff_thread.start()
+
+        while True:
+            if input() == 'q':
+                self.monitoring = False
+                break
+
+        sniff_thread.join()
+        self.finish()
 
     def finish(self):
-        if self.suspicious_packets:
-            print("Обнаруженные подозрительные пакеты:")
-            for ip, port in self.suspicious_packets:
-                print(f"IP {ip}, Порт {port}")
-        else:
+        if not self.suspicious_packets:
             print("Подозрительная активность не обнаружена.")
+
+        elif self.suspicious_packets:
+            while True:
+                user_input = input("Сохранить подозрительные пакеты? (y/n): ").lower()
+                if user_input == 'y':
+                    with open("suspicious_packets.json", "w") as file:
+                        json.dump(self.suspicious_packets, file, indent=4)
+                    print("Данные сохранены в файл 'suspicious_packets.json'.")
+                    break
+                elif user_input == 'n':
+                    print("Данные не сохранены.")
+                    break
+                else:
+                    print("Пожалуйста, введите 'y' для сохранения или 'n' для отказа.")
 
 
 if __name__ == "__main__":
     monitor = NetworkMonitor()
-    try:
-        monitor.start()
-    except KeyboardInterrupt:
-        print("Остановка мониторинга...")
-        monitor.finish()
+    monitor.start()
